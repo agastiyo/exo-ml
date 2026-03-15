7d59394bcf5f87d2f88af30c3eaf8fef7c7f9350
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2026-03-15 15:44:47 -0700

    Reorganize Small-Sample code

--- src/small-sample-PCA.py
+++ src/small-sample-PCA.py
@@ -5,11 +5,10 @@ from sklearn.preprocessing import StandardScaler
 
 #%%
 df = pd.read_csv('data/cleaned/STELLARHOSTS.csv',comment='#')
-print(df.shape)
 
-#%%
 # Check how many NaN values are in each column
 nanCheck = ['sy_pnum','sy_snum','st_teff','st_rad','st_mass','st_met_FeH','st_met_MH','st_met_NH','st_met_mH','st_lum','st_logg','st_age','st_dens','st_vsin','st_rotp','st_radv']
+nanDict = {}
 
 for col in nanCheck:
   nancount = 0
@@ -17,51 +16,37 @@ for col in nanCheck:
     if np.isnan(entry):
       nancount += 1
   percent = (nancount/len(df[col]))
+  nanDict[col] = percent
   print(f"{col:>10}: {nancount:>4} NaN ({percent:.2%})")
 
 #%%
+# Set parameters for running the small sample PCA
+tau = 0.7 # nan limit (col can have this percent of nans and still be used)
+
 # Create Matrix X for PCA
-Xt = np.array([df['sy_pnum'].to_numpy(),
-              df['sy_snum'].to_numpy(),
-              df['st_teff'].to_numpy(),
-              df['st_rad'].to_numpy(),
-              df['st_mass'].to_numpy(),
-              df['st_met_FeH'].to_numpy(),
-              #df['st_met_MH'].to_numpy(), redundant, too few entries in exoplanet hosts
-              #df['st_lum'].to_numpy(),
-              df['st_logg'].to_numpy(),
-              df['st_age'].to_numpy(),
-              df['st_dens'].to_numpy()
-              #df['st_vsin'].to_numpy(), Too few entries in Gaia DR3 and exoplanet hosts
-              #df['st_rotp'].to_numpy(), not included in Gaia DR3, too few entries in exoplanet hosts
-              #df['st_radv'].to_numpy(), not included in Gaia DR3, too few entries in exoplanet hosts
-              ])
+Xt = []
+for key in nanDict.keys():
+  if nanDict[key] <= tau:
+    Xt.append(df[key])
 
+Xt = np.array(Xt)
 X = Xt.T
-print(X)
-print(X.shape)
-# %%
-for i in range(X.shape[1]):
-  nancount = 0
-  for j in range(X.shape[0]):
-    if np.isnan(X[j][i]):
-      nancount += 1
-  print(f"{nancount}/{X.shape[0]} NaN in col {i}")
 
-# Optimal sample size should be 2^D.
-# %%
 # Try running PCA on a smaller dataset where only rows with all dimensions known are considered.
 X_known = X[np.isfinite(X).all(axis=1)]
 Xt_known = X_known.T
 print(X_known.shape)
-# The size of this dataset is also far larger than 256
+print(f"Optimal sample size for this dataset is {2**X_known.shape[1]}.")
+print(f"tau = {tau} yields {X_known.shape[0]} complete rows")
+if X_known.shape[0] == 0:
+  raise ValueError("No complete data to conduct PCA on!")
+
 # %%
 # Standardize each column in X_known and Xt_known before PCA
 scaler = StandardScaler()
 X_known_standard = scaler.fit_transform(X_known)
 Xt_known_standard = X_known_standard.T
 
-#%%
 # Compute the covariance matrix
 C_known = (1 / (X_known_standard.shape[1] - 1)) * (Xt_known_standard @ X_known_standard)
 
