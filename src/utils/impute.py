9d67376d9e56ff1dad9b6f66b97ec0e29ef5f8f2
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2026-03-20 01:22:41 -0700

    Implement efficient algorithm

--- src/utils/impute.py
+++ src/utils/impute.py
@@ -121,3 +121,128 @@ def PseudoGibbsImputer(X, X_initializer, regressor:RandomForestRegressor, save_d
     print(f"Iteration {i+1}/{tot_iters} done")
   
   return X,rmse_hist
+
+def EfficientPseudoGibbs(X, X_init,n_iters=15,burn_in=10,n_trees=30,max_depth=10,draws_per_iter=3,weight_str=1,stochastic_str=0.25):
+  # Due diligence
+  assert X.shape[1] == X_init.shape[1]
+  
+  X = X.copy()
+  output = []
+  num_features = X.shape[1]
+  
+  # Step 1: Create mask array
+  isImputed = np.isnan(X)
+  
+  # Step 2: Initialize from X_init
+  for currCol in range(num_features):
+    init_data = X_init[:,currCol]
+    init_data = init_data[~np.isnan(init_data)] # Get rid of NaN values
+    
+    nanRows = np.isnan(X[:, currCol]) # Mask of nan rows in this col
+    N = nanRows.sum() # Number of nan rows in this col
+    
+    draws = np.random.randint(0,len(init_data),N) # draws from the initial distribution
+    
+    X[nanRows,currCol] = init_data[draws] # Initialize the data matrix with the draws
+  
+  # Step 3: Define the bounds for each feature
+  bounds = {}
+  for currCol in range(num_features):
+    init_data = X_init[:,currCol]
+    init_data = init_data[~np.isnan(init_data)]
+    
+    og_data = X[:,currCol]
+    og_data = og_data[~np.isnan(og_data)]
+    
+    lo = min(np.min(init_data), np.min(og_data))
+    hi = max(np.max(init_data), np.max(og_data))
+    
+    bounds[currCol] = (lo, hi)
+  
+  # Step 4: Main iteration Loop
+  
+  # First, define the missing and observed indices for each feature:
+  missing_rows = [np.where(isImputed[:, i])[0] for i in range(num_features)]
+  observed_rows = [np.where(~isImputed[:, i])[0] for i in range(num_features)]
+  
+  # Next, define the predictor columns for each feature (X_-p)
+  predictor_cols = [np.delete(np.arange(num_features), i) for i in range(num_features)]
+  
+  # Next, define the random forests for each feature
+  forests = [ clone(RandomForestRegressor(n_estimators=n_trees, max_depth=max_depth, n_jobs=-1)) for _ in range(num_features) ]
+  
+  # Finally, the iteration
+  for i in range(n_iters):
+    print(f"Iteration {i+1}:")
+    # Train the random forest for each feature
+    for p in range(num_features):
+      mp = missing_rows[p]
+      op = observed_rows[p]
+      
+      if len(mp) == 0:
+        continue
+        
+      negp = predictor_cols[p]
+      
+      x_opp = X[op,p]
+      
+      X_opnegp = X[op][:,negp]
+      
+      rf = forests[p]
+      
+      isImputed_opnegp = isImputed[op][:, negp] # This is the mask matrix of X_opnegp
+      num_observed = (~isImputed_opnegp).sum(axis=1) # The number of observed values in each row
+      row_weights = num_observed / len(negp)
+      row_weights = row_weights ** weight_str # Scale the strength of the row weights
+      row_weights = np.clip(row_weights, 1e-6, 1) # A weight of 0 won't work, so a very small offset is added. Max should be 1
+      
+      rf.fit(X_opnegp, x_opp, sample_weight=row_weights)
+      
+      # The random forest is now trained for this feature!
+    
+    print("    Trained!")
+    
+    # Now do the draws
+    for k in range(draws_per_iter):
+      for p in range(num_features):
+        mp = missing_rows[p]
+        op = observed_rows[p]
+        
+        if len(mp) == 0:
+          continue
+        
+        negp = predictor_cols[p]
+        
+        rf = forests[p]
+        
+        x_mpp = X[mp,p]
+        X_mpnegp = X[mp][:,negp]
+        
+        xPredicted_mpp = rf.predict(X_mpnegp)
+        
+        # Standard deviation scaled by stochastic strength
+        sigma = np.sqrt(np.mean( (x_mpp - xPredicted_mpp) ** 2 ))
+        sigma *= stochastic_str
+        sigma = max(sigma, 1e-6)
+        
+        # The new values are drawn from a truncated normal
+        lo, hi = bounds[p]
+        
+        a = (lo - xPredicted_mpp) / sigma
+        b = (hi - xPredicted_mpp) / sigma
+
+        yNew_mpp = truncnorm.rvs(a, b, loc=xPredicted_mpp, scale=sigma)
+        
+        # The new values 
+        X[mp,p] = yNew_mpp
+      
+      # Save to the output
+      output.append(X.copy())
+    
+      print(f"    {k+1}/{draws_per_iter} draws complete")
+    
+  # Done with the iteration
+  # Remove the burn in iterations
+  output = output[burn_in:]
+  
+  return output
